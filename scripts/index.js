b3973c4d26f8063bb4a2b255e6fb3f995f8d3167
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-07 11:19:25 -0800

    simplify store update

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,20 @@ $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
   api.getItems((items) => {
-    console.log(items);
+    // console.log(items);
     items.forEach((item) => store.addItem(item));
+    const item = store.items[0];
+    console.log('current name: ' + item.name);
+    store.findAndUpdate(item.id, { name: 'foobar' });
+    console.log('new name: ' + item.name);
     shoppingList.render();
   });
 });
 
 
-// api.createItem('pears', (newItem) => {
-//   api.getItems((items) => {
-//     console.log(items);
+// api.getItems((items) => {
+//   const item = items[0];
+//   api.updateItem(item.id, { checked: true }, () => {
+//     console.log('updated!');
 //   });
 // });
--- scripts/store.js
+++ scripts/store.js
@@ -12,24 +12,29 @@ const store = (function(){
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
+  const findAndUpdate = function(id, newData) {
     const item = this.findById(id);
-    item.checked = !item.checked;
+    Object.assign(item, newData);
   };
+
+  // const findAndToggleChecked = function(id) {
+  //   const item = this.findById(id);
+  //   item.checked = !item.checked;
+  // };
 
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
   };
 
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
-  };
+  // const findAndUpdateName = function(id, name) {
+  //   try {
+  //     Item.validateName(name);
+  //     const item = this.findById(id);
+  //     item.name = name;
+  //   } catch(e) {
+  //     console.log('Cannot update name: ' + e.message);
+  //   }
+  // };
 
   const toggleCheckedFilter = function() {
     this.hideCheckedItems = !this.hideCheckedItems;
@@ -51,12 +56,13 @@ const store = (function(){
 
     addItem,
     findById,
-    findAndToggleChecked,
+    // findAndToggleChecked,
     findAndDelete,
-    findAndUpdateName,
+    // findAndUpdateName,
     toggleCheckedFilter,
     setSearchTerm,
     setItemIsEditing,
+    findAndUpdate
   };
   
 }());
